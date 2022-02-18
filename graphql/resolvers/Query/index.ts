8be77ab37652470b0c544ee11f7eb4210223d6b9
hfend <simplizine@gmail.com>
2022-02-18 16:45:40 +0100

    Update gql typeDefs and resolvers to be 1to1 with db Added missing announcement type Added missing timestamp fields

--- graphql/resolvers/Query/index.ts
+++ graphql/resolvers/Query/index.ts
@@ -1,5 +1,7 @@
+import _announcement from './announcement'
 import _project from './project'
 import _user from './user'
 
+export const announcement = _announcement
 export const project = _project
 export const user = _user
--- graphql/server.ts
+++ graphql/server.ts
@@ -2,11 +2,22 @@ import {ApolloServer, gql} from 'apollo-server-micro'
 import * as resolvers from './resolvers'
 
 const typeDefs = gql`
+  type Announcement {
+    id: Int!
+    fellowship: String!
+    title: String!
+    body: String!
+    created_ts: String!
+    updated_ts: String!
+  }
+
   type Project {
     id: Int!
     name: String!
     description: String!
     icon_url: String!
+    created_ts: String!
+    updated_ts: String!
     users: [User!]!
   }
 
@@ -16,10 +27,13 @@ const typeDefs = gql`
     bio: String!
     avatar_url: String!
     fellowship: String!
+    created_ts: String!
+    updated_ts: String!
     projects: [Project!]!
   }
 
   type Query {
+    announcement(id: Int!): Announcement!
     project(id: Int!): Project!
     user(id: Int!): User!
   }
